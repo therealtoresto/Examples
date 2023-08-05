a91f2183c7d8282c8da3e6c1c57d4493cc49e1a0
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-08-05 15:49:12 +0200

    update

--- nodejs/2-console/6-tic-tac-toe.js
+++ nodejs/2-console/6-tic-tac-toe.js
@@ -1,71 +1,88 @@
 /* eslint-disable */
 const readline = require('node:readline');
-const fs = require('node:fs');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-const resultFile = 'result.txt';
+const board = [
+    ['X', 'X', 'X'],
+    ['4', '5', '6'],
+    ['7', '8', '9']
+];
 
-const choises = [ 'rock', 'paper', 'scissors' ];
+let currentPlayer = 'X';
+let moves = 0;
 
-const getRandomChoise = () => {
-    return choises[Math.floor(Math.random() * choises.length)]; 
-};
+const printBoard = () => {
+    console.log('\n');
+    for (let row of board) {
+        console.log(row.join(' | '));
+        console.log(('--------'));
+    }
+    console.log('\n');
+}
 
-const determineWinner = (userChoise, computerChoise) => {
-    if (userChoise === computerChoise) {
-        return `It's a tie!`;
+const checkWinner = () => {
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+            return true;
+        }
     }
 
-    const winMap = new Map([
-        ['rock', 'scissors'],
-        ['paper', 'rock'],
-        ['scissors', 'paper']
-    ]);
-    console.log('userChoise:', winMap.get(userChoise));
-
-    if (winMap.get(userChoise) === computerChoise) {
-        return 'You win!';
-    } else {
-        return 'Computer win!';
+    // Check cols
+    for (let j = 0; j < 3; j++) {
+        if (board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
+            return true;
+        }
     }
-};
+
+    // Check diagonals
+    if (
+        (board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+        (board[0][2] === board[1][1] && board[1][1] === board[2][0])
+    ) {
+        return true;
+    }
+
+    return false;
+}
 
 const playGame = () => {
-    rl.question('Choose rock, paper of scissors: ', (userChoise) => {
-        userChoise = userChoise.toLowerCase();
+    printBoard();
 
-        if (!choises.includes(userChoise)) {
-            console.log('Invalid choise. Please choose rock, paper or scissors.');
+    rl.question(`${currentPlayer}'s turn. Enter the number where you want to place your symbol: `, (input) => {
+        const position = parseInt(input) - 1;
+        const row = Math.floor(position / 3);
+        const col = position % 3;
+
+        if (isNaN(position) || position < 0 || position > 8 || board[row][col] === 'X' || board[row][col] === '0') {
+            console.log('Invalid move. Please choose an available number from the board.');
             playGame();
             return;
         }
 
-        const computerChoise = getRandomChoise();
-        console.log('Computer choose:', computerChoise);
+        board[row][col] = currentPlayer;
+        moves++;
 
-        const result = determineWinner(userChoise, computerChoise);
-        console.log(result);
+        if (checkWinner()) {
+            printBoard();
+            console.log(`${currentPlayer} wins!`);
+            rl.close();
+            return;
+        } else if (moves === 9) {
+            printBoard();
+            console.log('It`s a tie!');
+            rl.close();
+            return;
+        }
 
-        fs.appendFile(resultFile, `${computerChoise} vs ${userChoise} => ${result}`, (err) => {
-            if (err) {
-                console.log('Error writing to file', err);
-            }
-        })
-
-        rl.question('Do you want to play again? (yes/no): ', (answer) => {
-            if (answer.toLowerCase() === 'yes' || 'y') {
-                playGame();
-            } else {
-                console.log('Thanks for playing!');
-                rl.close();
-            }
-        });
-    });
+        currentPlayer = currentPlayer === 'X' ? '0' : 'X';
+        playGame();
+    })
 }
 
-console.log('Welcome to Rock-Paper-Scissors Game!');
+console.log('Welcome to Tic Tec Toe!');
 playGame();
